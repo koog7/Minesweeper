dbb2fa9077e573c9c0d22b3096d3f5f3a4700190
koog7 <k.ognev709@mail.com>
2024-05-26 12:56:57 +0600

    Создание проекта vite react-ts

---
+++ src/Components/Cell.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Cell = () => {
+    return (
+        <div>
+            <a href="#" className="game-cell">Клетка</a>
+        </div>
+    );
+};
+
+export default Cell;
